5505628c259c4c12287403c9b52709049d4c032a
EvanCharles16 <evancharles1609@gmail.com>
2020-07-10 20:28:36 +0700

    change logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,14 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+import logoBrand from "../../img/logoBrand.png";
+
 class navbar extends Component {
   render() {
     return (
       <Navbar bg="danger" variant="dark" expand="lg" fixed="top">
         <Navbar.Brand to="#" className="navbarNav ml-3">
-          KOS45
+          <img src={logoBrand} alt="logoBrand" className="logoBrand" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
