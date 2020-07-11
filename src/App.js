9a8ae8f289cfc5ac1e4955f0bf934c462db8df7a
Frederick-88 <frederickfd88@gmail.com>
2020-07-11 19:08:14 +0700

    tes pullreq

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
-import Home from './pages/Home/Main/Main'
+import Home from "./pages/Home/Main/Main";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="" component={Home} />
+        <Route path="/" component={Home} />
       </Switch>
       <Footer />
     </Router>
